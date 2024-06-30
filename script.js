c668c7bd47b5655e76c20914ab5a1f2a61060ba0
FREELANCERMDG <anthony.eglmada@gmail.com>
2024-06-30 12:09:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Exemple de fonctionnalité pour le formulaire de contact
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        alert('Merci de nous avoir contactés. Nous reviendrons vers vous sous peu.');
+    });
+});
